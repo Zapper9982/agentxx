697632debe99d19a2bc034c28a7b52e99f842d59
Aryan Pandit <aryanpandit.1205@gmail.com>
2025-03-15 22:46:51 +0530

    multiplying_rag usecases

--- agentx/content_update.py
+++ agentx/content_update.py
@@ -1,38 +1,16 @@
 import json
-import requests
 import os
-import faiss
-import numpy as np
 import re
-from dotenv import load_dotenv
-from datetime import datetime
-from sentence_transformers import SentenceTransformer
-
 import google.generativeai as genai
+from dotenv import load_dotenv
+from rag_utils import vec_store, retrieval
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-
-
-embedding_model = SentenceTransformer('all-MiniLM-l6-v2')
-
 def load_data(file_path):
     with open(file_path, "r", encoding="utf-8") as f:
         return json.load(f)
-    
-def vec_store(sdata):
-    texts = [item["content"] for item in sdata]
-    embeddings = embedding_model.encode(texts, convert_to_numpy=True)
-    index = faiss.IndexFlatL2(embeddings.shape[1])
-    index.add(embeddings)
-    return index, embeddings, texts
-
-def retrieval(index, embeddings, texts, query_text, top=3):
-    query_embedding = embedding_model.encode([query_text])
-    distances, indices = index.search(query_embedding, top)
-    retrieved = [texts[idx] for idx in indices[0]]
-    return "\n\n".join(retrieved)
 
 
 
